964919b6ed6f4a21d99ac7c1e075b025d66cf98b
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-14 23:45:39 -0300

    Add files via upload
    Reenvio da tarefa sobre UML - Adição e Subtração de produtos, utilizando método ToString

---
+++ Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ProdutoEstoqueTS
+{
+    class Produto
+    {
+        public string Product;
+        public double Price;
+        public int Qty;
+
+        //Método para cálculo do valor total em estoque
+        public double TotalValueStock()
+        {
+            return Price * Qty;
+        }
+
+        //Método para adicionar produtos
+        public void AddProduct(int qty)
+        {
+            Qty += qty;
+        }
+
+        //Método para remover produtos
+        public void RemoveProduct(int qty)
+        {
+            Qty -= qty;
+        }
+
+        //Método ToString para inserir todas as informações unificadas
+        public override string ToString()
+        {
+            return Product
+            + ",$"
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + " " + Qty
+            + " unidades, Total: $ "
+            + TotalValueStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutoEstoqueTS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            //Recebimento das informações
+            Console.Write("Informe o nome do produto: ");
+            p.Product = Console.ReadLine();
+            Console.Write("Informe o preço do produto: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Informe a quantidade em estoque: ");
+            p.Qty = int.Parse(Console.ReadLine());
+
+            //Exibição das informações iniciais
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine(" ");
+
+            //Adição de produtos
+            Console.Write("Informe a quantidade de produtos a serem adicionados: ");
+            int qtt = int.Parse(Console.ReadLine());
+            p.AddProduct(qtt);
+
+            //Exibição das informações atualizadas após adição
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine(" ");
+
+            //Subtração de produtos
+            Console.Write("Informe a quantidade de produtos a serem removidos:");
+            qtt = int.Parse(Console.ReadLine());
+            p.RemoveProduct(qtt);
+
+            //Exibição das informações atualizadas após subtração
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
